4ff24f33b512fc7831e2959f1b843cd14577ee09
Xian <josiexianli@gmail.com>
2017-10-15 01:16:17 -0700

    project

---
+++ XLI_StudentInformation_Relative/app/src/main/java/com/xli/xli_studentinformation_relative/StudentInformationFragment.java
@@ -0,0 +1,22 @@
+package com.xli.xli_studentinformation_relative;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class StudentInformationFragment extends Fragment {
+
+    public StudentInformationFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_student_information, container, false);
+    }
+}
